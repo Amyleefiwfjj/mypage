bf372a8a8be11df3bbf8fa689ad0de1c16042bcf
이다은 <162148102+Amyleefiwfjj@users.noreply.github.com>
2024-07-28 14:52:32 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
+import Dashboard from './pages/Dashboard/Dashboard';
+/*import my-page from './pages/Dashboard/Dashboard';*/
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+{/*       <Route path="/my_page" element={<my_page />} />*/}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
